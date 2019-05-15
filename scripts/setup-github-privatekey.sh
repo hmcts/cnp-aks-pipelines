7d73bb37416fe2a01441b468a4285cbcbef08ece
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-05-15 08:37:58 +0100

    Moving to script

---
+++ scripts/setup-github-privatekey.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+mkdir -p ~/.ssh
+echo "$(flux-github-private-key)" > ~/.ssh/id_rsa
+echo "Setting up appropriate permissiions for ssh key"
+chmod 0600 ~/.ssh/id_rsa
+ls -al ~/.ssh
+echo "Attempting key scan"
+ssh-keyscan github.com github.com >> ~/.ssh/known_hosts
+ssh-keygen -F github.com -f  ~/.ssh/known_hosts # verifies key is correctly installed
+git config --global user.name $(flux-github-username)
+git config --global url."git@github.com:".insteadOf "https://github.com/"
