e1f7f9c11662199ddac0bb92f5f439880d13116f
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-05-15 10:07:54 +0100

    cleaning parameters

--- scripts/setup-github-privatekey.sh
+++ scripts/setup-github-privatekey.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 mkdir -p ~/.ssh
-echo "$(GITHUB_KEY)" > ~/.ssh/id_rsa
+echo "${GITHUB_KEY}" > ~/.ssh/id_rsa
 echo "Setting up appropriate permissiions for ssh key"
 chmod 0600 ~/.ssh/id_rsa
 ls -al ~/.ssh
 echo "Attempting key scan"
 ssh-keyscan github.com github.com >> ~/.ssh/known_hosts
 ssh-keygen -F github.com -f  ~/.ssh/known_hosts # verifies key is correctly installed
-git config --global user.name $(GITHUB_USERNAME)
+git config --global user.name ${GITHUB_USERNAME}
 git config --global url."git@github.com:".insteadOf "https://github.com/"
