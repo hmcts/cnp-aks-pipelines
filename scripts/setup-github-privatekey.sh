8ded8e10d812bfa6071c86367301f1f9e431d741
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2019-05-17 10:58:04 +0100

    Fixing git config for flux user (#54)
    * Fixing git config.

--- scripts/setup-github-privatekey.sh
+++ scripts/setup-github-privatekey.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# setup env with GITHUB_KEY with github private key and GITHUB_USERNAME with username to set agent with credentials.
+# setup env with GITHUB_KEY with github private key and GITHUB_USER_NAME, GITHUB_USER_EMAIL with user details to set agent with credentials.
 mkdir -p ~/.ssh
 echo "${GITHUB_KEY}" > ~/.ssh/id_rsa
 echo "Setting up appropriate permissiions for ssh key"
@@ -8,5 +8,6 @@ ls -al ~/.ssh
 echo "Attempting key scan"
 ssh-keyscan github.com github.com >> ~/.ssh/known_hosts
 ssh-keygen -F github.com -f  ~/.ssh/known_hosts # verifies key is correctly installed
-git config --global user.name ${GITHUB_USERNAME}
+git config --global user.name ${GITHUB_USER_NAME}
+git config --global user.email ${GITHUB_USER_EMAIL}
 git config --global url."git@github.com:".insteadOf "https://github.com/"
