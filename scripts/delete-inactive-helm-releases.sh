13d933eba90d8b455a849adf7b0b9c7fa1b37cb4
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-09-27 13:32:44 +0100

    fixing review comments

--- scripts/delete-inactive-helm-releases.sh
+++ scripts/delete-inactive-helm-releases.sh
@@ -7,14 +7,14 @@ for ns in $(echo "am bsp camunda ccd chart-tests cnp ctsc dg divorce em ethos ev
 
   #Encoding and decoding to base64 is to handle spaces in updated field of helm ls command.
   for release in $(echo "${helmreleases}" | jq -r '.[] | @base64'); do
-    #uses gdate to convert to epoch millis.
+    #uses gdate to convert to epoch millis. Install gdate while running on mac.
       lastUpdated=$(date -d "$(echo $release| base64 --decode | jq -r '.Updated')"  +%s)
       releaseName=$(echo $release| base64 --decode | jq -r '.Name')
       currenttime=$(date +%s)
       cutoff=$((inactiveDays*24*3600))
       if [ $((currenttime-lastUpdated)) -gt "$cutoff" ]
        then
-         echo "Deleting Helm release ${releaseName} as it is inactive for more than ${inactiveDays} days"
+         echo "Deleting Helm release ${releaseName} as it is inactive for more than ${inactiveDays} days. LastUpdated :  $(echo "$release"| base64 --decode | jq -r '.Updated') "
          helm delete --purge "${releaseName}"
       fi
   done
