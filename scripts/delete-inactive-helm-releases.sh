fe13f94dae458ad97f91b35c3d746fdd1a3305b7
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-09-27 12:14:43 +0100

    Adding job to delete old helm releases not updated for a week on preview

---
+++ scripts/delete-inactive-helm-releases.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+for ns in $(echo "am bsp camunda ccd chart-tests cnp ctsc dg divorce em ethos evidence-mment family-public-law fees-pay financial-remedy ia idam immigration money-claims probate professional-applications rd rpe sscs"); do
+  helmreleases=$(helm ls --namespace=${ns} --output=json | jq '.Releases')
+  inactiveDays=${1:-7}
+
+  for release in $(echo "${helmreleases}" | jq -r '.[] | @base64'); do
+      lastUpdated=$(gdate -d "$(echo $release| base64 --decode | jq -r '.Updated')"  +%s)
+      releaseName=$(echo $release| base64 --decode | jq -r '.Name')
+      currenttime=$(gdate +%s)
+      cutoff=$((inactiveDays*24*3600))
+      if [ $((currenttime-lastUpdated)) -gt "$cutoff" ]
+       then
+         echo "Deleting Helm release ${releaseName} as it is inactive for more than ${inactiveDays} days"
+         echo ${releaseName}
+      fi
+  done
+done
+
+
+
+
