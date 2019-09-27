f1853f3b4bf090a662f61e26f071ee59a84ee4e2
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2019-09-27 14:04:00 +0100

    Adding job to delete helm releases not updated for more than a week  (#74)

---
+++ scripts/delete-inactive-helm-releases.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+for ns in $(echo "am bsp camunda ccd chart-tests cnp ctsc dg divorce em ethos evidence-mment family-public-law fees-pay financial-remedy ia idam immigration money-claims probate professional-applications rd rpe sscs"); do
+  helmreleases=$(helm ls --namespace=${ns} --output=json | jq '.Releases')
+  inactiveDays=${1:-7}
+
+  # Encoding and decoding to base64 is to handle spaces in updated field of helm ls command.
+  for release in $(echo "${helmreleases}" | jq -r '.[] | @base64'); do
+    # Uses gnu date to convert to epoch millis, install gdate while running on mac.
+      lastUpdated=$(date -d "$(echo $release| base64 --decode | jq -r '.Updated')"  +%s)
+      releaseName=$(echo $release| base64 --decode | jq -r '.Name')
+      currenttime=$(date +%s)
+      cutoff=$((inactiveDays*24*3600))
+      if [ $((currenttime-lastUpdated)) -gt "$cutoff" ]
+       then
+         echo "Deleting Helm release ${releaseName} as it is inactive for more than ${inactiveDays} days. Last updated :  $(echo "$release"| base64 --decode | jq -r '.Updated') "
+         helm delete --purge "${releaseName}"
+      fi
+  done
+done
+
+
+
+
