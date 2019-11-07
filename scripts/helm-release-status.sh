21e33316162f54c2a6e002938a4d3862faa51606
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2019-11-07 22:54:05 +0000

    try as variable

--- scripts/helm-release-status.sh
+++ scripts/helm-release-status.sh
@@ -45,7 +45,8 @@ for ns in $(echo ${!namespaceMapping[*]}); do
   if [ ! -z "$failedReleaseNames" ]
   then
         echo "Sending notification to slack channel ${namespaceMapping[$ns]} for $failedReleaseNames"
-        curl -X POST --data-urlencode 'payload={"channel": "${namespaceMapping[$ns]}", "username": "${CLUSTER_NAME}", "text": "Helm releases are in a failed state: $failedReleaseNames", "icon_emoji": ":rotating_light:"}' ${WEBHOOK_URL}
+        build_notices_channel=${namespaceMapping[$ns]}
+        curl -X POST --data-urlencode 'payload={"channel": "${build_notices_channel}", "username": "${CLUSTER_NAME}", "text": "Helm releases are in a failed state: $failedReleaseNames", "icon_emoji": ":rotating_light:"}' ${WEBHOOK_URL}
   fi
 
 done
