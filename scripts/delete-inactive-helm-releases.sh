47806d4f8d4fd2270295f1498f162b4daba6732c
Tim Jacomb <tim.jacomb@hmcts.net>
2020-01-13 12:12:03 +0000

    Fix cleanup (#98)

--- scripts/delete-inactive-helm-releases.sh
+++ scripts/delete-inactive-helm-releases.sh
@@ -2,20 +2,27 @@
 set -e
 
 for ns in $(echo "am bsp camunda ccd chart-tests cnp ctsc dg divorce em ethos evidence-mment family-public-law fees-pay financial-remedy ia idam immigration money-claims probate professional-applications rd rpe sscs"); do
-  helmreleases=$(helm ls --namespace=${ns} --output=json | jq '.Releases')
+  helmreleases=$(helm ls --namespace=${ns} --output=json)
   inactiveDays=${1:-7}
 
   # Encoding and decoding to base64 is to handle spaces in updated field of helm ls command.
   for release in $(echo "${helmreleases}" | jq -r '.[] | @base64'); do
+      fullDate=$(echo $release| base64 --decode | jq -r '.updated')
+      # remove 'UTC' string, for some reason helm adds utc offset and timezone name which breaks parsing
+      date=${fullDate%????}
+
     # Uses gnu date to convert to epoch millis, install gdate while running on mac.
-      lastUpdated=$(date -d "$(echo $release| base64 --decode | jq -r '.Updated')"  +%s)
-      releaseName=$(echo $release| base64 --decode | jq -r '.Name')
+      lastUpdated=$(date -d "${date}"  +%s)
+      releaseName=$(echo $release| base64 --decode | jq -r '.name')
       currenttime=$(date +%s)
       cutoff=$((inactiveDays*24*3600))
       if [ $((currenttime-lastUpdated)) -gt "$cutoff" ]
        then
-         echo "Deleting Helm release ${releaseName} as it is inactive for more than ${inactiveDays} days. Last updated :  $(echo "$release"| base64 --decode | jq -r '.Updated') "
-         helm delete --purge "${releaseName}"
+         echo "Deleting helm release ${releaseName} as it is inactive for more than ${inactiveDays} days. Last updated : ${date} "
+         helm delete --namespace "${ns}" "${releaseName}"
+#         Enable for debug if needed
+#        else
+#          echo "Skipping ${releaseName} as it is not inactive for ${inactiveDays}, Last updated: ${date}, cutoff=${cutoff}, result=$((currenttime-lastUpdated))"
       fi
   done
 done
