e35d260c7a4624d3d1b4811e547fa5742c770815
Tim Jacomb <tim.jacomb@hmcts.net>
2020-06-12 15:08:53 +0100

    RPE-1227 Product with - fix

---
+++ scripts/untag-acr-image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RELEASE_NAME=${1}
+PRODUCT=${2}
+CONTAINER_REGISTRY=${3}
+
+component=$(echo ${RELEASE_NAME} | sed -e "s/^${PRODUCT}-//" -e 's/-pr-.*//' -e '1 s/-/\//')
+repository="${PRODUCT}/${component}"
+tag=$(echo ${RELEASE_NAME} | sed "s/.*-pr-/pr-/")
+
+echo "Deleting $repository:$tag"
+
+az acr repository show-tags -n ${CONTAINER_REGISTRY} --repository $repository --query "[?starts_with(@, '$tag')]" -o tsv \
+| xargs -I% az acr repository untag -n ${CONTAINER_REGISTRY} --image "$repository:%"
+echo "Deleted $repository:$tag"
