abd363a4e1c0cfd4b0ae376290b0dfc6e4dcb18d
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-07-03 08:22:50 +0100

    Update scripts/clean-pr-release.sh
    Co-authored-by: Tim Jacomb <tim.jacomb@hmcts.net>

--- scripts/clean-pr-release.sh
+++ scripts/clean-pr-release.sh
@@ -4,7 +4,7 @@ kubernetesCluster=$2
 namespace=$3
 release_name=$4
 
-az aks get-credentials --resource-group ${azureResourceGroup} --name ${kubernetesCluster} -a || exit 0
+az aks get-credentials --resource-group ${azureResourceGroup} --name ${kubernetesCluster} -a || echo "Cluster ${kubernetesCluster} not found in ${azureResourceGroup}"
 if [ "$(helm get values --namespace "${namespace}" "${release_name}" 2>/dev/null)" ]; then
   helm delete --namespace ${namespace} ${release_name}
 else
