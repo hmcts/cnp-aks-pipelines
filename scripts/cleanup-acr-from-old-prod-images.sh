8f411c2ef69482e0d25d0748efeab826b5e0e163
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-09-08 10:21:02 +0100

    Update cleanup-acr-from-old-prod-images.sh (#130)

--- scripts/cleanup-acr-from-old-prod-images.sh
+++ scripts/cleanup-acr-from-old-prod-images.sh
@@ -19,19 +19,13 @@ do
     continue
   fi
 
-  # ACR API doesn't respect the 'last' parameter on our base images when passing the sortby flag
-  if [[ $repo =~ (^base/.*|^cmc/ccd-definition-importer$|^imported/.*|k8s-dns-node-cache-amd64|node-load|^rpe/auto-reply-urls$|rse/check|sscs/ccd-definition-importer-benefit|ignoretest/alpine|timj/auto-.*) ]]; then
-    echo "$(TERM=xterm tput setaf 3)Skipping $repo as it's a base image"
-    continue
-  fi
-
   echo "$(TERM=xterm tput setaf 2)Cleaning up $repo, deleting images older than $older_than and keeping at least $keep_min_latest_num"
 
   cat << EOF | az acr run --registry "$registry" -f - --timeout 10800 /dev/null
 version: v1.1.0
 alias:
   values:
-    forkedacr: "hmctspublic.azurecr.io/acr:fd0cf6"
+    forkedacr: "hmctspublic.azurecr.io/acr:6e655c8"
 steps:
   - cmd: \$forkedacr purge --registry \$RegistryName --filter $repo:^prod-.* --keep ${keep_min_latest_num} --ago ${older_than}
   - cmd: \$forkedacr purge --registry \$RegistryName --filter $repo:^aat-.* --keep ${keep_min_latest_num} --ago ${older_than}
