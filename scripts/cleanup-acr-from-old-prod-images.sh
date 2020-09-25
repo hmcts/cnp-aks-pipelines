9ae1017712368fb922694684ccd58f33dc3b716e
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-09-25 14:44:47 +0100

    Use latest main branch (#132)

--- scripts/cleanup-acr-from-old-prod-images.sh
+++ scripts/cleanup-acr-from-old-prod-images.sh
@@ -25,7 +25,7 @@ do
 version: v1.1.0
 alias:
   values:
-    forkedacr: "hmctspublic.azurecr.io/acr:6e655c8"
+    forkedacr: "hmctspublic.azurecr.io/acr:e666d99"
 steps:
   - cmd: \$forkedacr purge --registry \$RegistryName --filter $repo:^prod-.* --keep ${keep_min_latest_num} --ago ${older_than} --untagged
   - cmd: \$forkedacr purge --registry \$RegistryName --filter $repo:^aat-.* --keep ${keep_min_latest_num} --ago ${older_than} --untagged
