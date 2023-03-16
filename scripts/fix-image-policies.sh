87c519341f7e266c4339649883d020b336092f64
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-03-16 16:46:45 +0000

    try

--- scripts/fix-image-policies.sh
+++ scripts/fix-image-policies.sh
@@ -47,7 +47,7 @@ for REPO_FILE in $(grep -Elr "kind: ImageRepository"  apps/ | xargs grep -El "$R
 
     if [[ -n $(git status -s) ]]
     then
-      git config --global user.email github-platform-operations@HMCTS.NET & git config --global user.name "hmcts-platform-operations"
+      git config --list
       git add .
       git commit -m "Removing $TAG image policy from $POLICY_FILE"
       git push --set-upstream origin HEAD:master
