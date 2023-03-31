afb256ed80f92d2c5315cc790e246d18a30c8472
Tim Jacomb <timjacomb1@gmail.com>
2023-03-31 11:39:20 +0100

    WIP fix cleanup

--- scripts/cleanup-acr-old-images.sh
+++ scripts/cleanup-acr-old-images.sh
@@ -14,36 +14,36 @@ repo_regex="${1:-.*}"
 registry=${2:-hmctspublic}
 older_than=30d
 keep_min_latest_num=6
-pr_old_than=5d
+nonprod_older_than=5d
 
-az acr repository list --name "$registry" -o tsv \
-  | while read repo
-do
-  if ! [[ $repo =~ $repo_regex ]]; then
-    continue
-  fi
+# az acr repository list --name "$registry" -o tsv \
+#   | while read repo
+# do
+#   if ! [[ $repo =~ $repo_regex ]]; then
+#     continue
+#   fi
 
   echo "$(TERM=xterm tput setaf 2)Cleaning up $repo, deleting master images older than $older_than and keeping at least $keep_min_latest_num, pr images older than $pr_old_than"
 
   cat << EOF | az acr run --registry "$registry" -f - --timeout 10800 /dev/null
 version: v1.1.0
 steps:
-  - cmd: acr purge --registry \$RegistryName --filter $repo:^prod-.* --keep ${keep_min_latest_num} --ago ${older_than} --untagged --concurrency 5
+  - cmd: acr purge --registry \$RegistryName --filter .*:^prod-.* --keep ${keep_min_latest_num} --ago ${older_than} --untagged --concurrency 5
     retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter $repo:^aat-.* --keep ${keep_min_latest_num} --ago ${older_than} --untagged --concurrency 5
+  - cmd: acr purge --registry \$RegistryName --filter .*:^aat.* --ago 0.5h --untagged --concurrency 5
     retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter $repo:^staging-.* --keep ${keep_min_latest_num} --ago ${older_than} --untagged --concurrency 5
+  - cmd: acr purge --registry \$RegistryName --filter .*:^staging.* --ago 0.5h --keep 1 --untagged --concurrency 5
     retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter $repo:^demo-.* --keep ${keep_min_latest_num} --ago ${older_than} --untagged --concurrency 5
+  - cmd: acr purge --registry \$RegistryName --filter .*:^demo.* --ago 0.5h --untagged --concurrency 5
     retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter $repo:^ithc-.* --keep ${keep_min_latest_num} --ago ${older_than} --untagged --concurrency 5
+  - cmd: acr purge --registry \$RegistryName --filter .*:^ithc.* --ago 0.5h --untagged --concurrency 5
     retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter $repo:^perftest-.* --keep ${keep_min_latest_num} --ago ${older_than} --untagged --concurrency 5
+  - cmd: acr purge --registry \$RegistryName --filter .*:^perftest.* --ago 0.5h --untagged --concurrency 5
     retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter $repo:^sandbox-.* --keep ${keep_min_latest_num} --ago ${older_than} --untagged --concurrency 5
+  - cmd: acr purge --registry \$RegistryName --filter .*:^sandbox-.* --keep ${keep_min_latest_num} --ago ${nonprod_older_than} --untagged --concurrency 5
     retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter $repo:^pr-.* --ago ${pr_old_than} --untagged --concurrency 5
+  - cmd: acr purge --registry \$RegistryName --filter .*:^pr-.* --ago ${nonprod_older_than} --untagged --concurrency 5
     retries: 3
 EOF
   
-done
+# done
