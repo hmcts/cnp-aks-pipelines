1c47da61092094f947aa88f0ac089c371db39386
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-05-09 10:58:10 +0100

    fix job name and typo

--- scripts/cleanup-acr-old-images.sh
+++ scripts/cleanup-acr-old-images.sh
@@ -11,6 +11,6 @@ older_than="${4:-30d}"
 keep_min_latest_num="${5:-5}"
 
 
-echo "$(TERM=xterm tput setaf 2)Cleaning up $registry, deleting ${tag_filter} images older than $older_than abd keeping at least $keep_min_latest_num"
+echo "$(TERM=xterm tput setaf 2)Cleaning up $registry, deleting ${tag_filter} images older than $older_than and keeping at least $keep_min_latest_num"
 
 az acr run --registry $registry --cmd "acr purge --registry \$RegistryName --filter ${repo_regex}:${tag_filter} --ago ${older_than} --keep ${keep_min_latest_num}  --untagged --concurrency 5" --timeout 10800  /dev/null
