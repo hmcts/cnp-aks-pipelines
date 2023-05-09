aeef0c8f6831ca1264d16306f891054f2ee84013
adusumillipraveen <adusumillipraveenkumar@gmail.com>
2023-05-09 10:40:06 +0100

    split into multiple jobs

--- scripts/cleanup-acr-old-images.sh
+++ scripts/cleanup-acr-old-images.sh
@@ -1,49 +1,16 @@
 #!/usr/bin/env bash
 
-# Clean up old prod, aat and staging images from an ACR registry. The images selected for removal have a tag starting 
-# with one of the following strings: 'prod-', 'aat-', 'staging-', 'demo-', 'ithc-', 'perftest-', 'sandbox-'
-# All such images older than 30 days are deleted, but at least the most recent 6 images will always be kept.  
-# A repo filter can be added otherwise the entire registry is scanned.
-
-# Clean up old PR images from an ACR registry. The images selected for removal have a tag starting with 'pr-' 
-# By default all such images are older than 5 days. 
+# Clean up old images based on input parameters.
 
 set -e
 
-repo_regex="${1:-.*}"
-registry=${2:-hmctspublic}
-older_than=30d
-keep_min_latest_num=6
-nonprod_older_than=5d
+registry=${1:-hmctspublic}
+repo_regex="${2:-.*}"
+tag_filter="${3:-^ignore-.*}"
+older_than="${4:-30d}"
+keep_min_latest_num="${4:-5}"
 
-# az acr repository list --name "$registry" -o tsv \
-#   | while read repo
-# do
-#   if ! [[ $repo =~ $repo_regex ]]; then
-#     continue
-#   fi
 
-  echo "$(TERM=xterm tput setaf 2)Cleaning up $repo, deleting master images older than $older_than and keeping at least $keep_min_latest_num, pr images older than $pr_old_than"
+echo "$(TERM=xterm tput setaf 2)Cleaning up $registry, deleting ${tag_filter} images older than $older_than abd keeping at least $keep_min_latest_num"
 
-  cat << EOF | az acr run --registry "$registry" -f - --timeout 10800 /dev/null
-version: v1.1.0
-steps:
-  - cmd: acr purge --registry \$RegistryName --filter .*:^prod-.* --keep ${keep_min_latest_num} --ago ${older_than} --untagged --concurrency 5
-    retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter .*:^aat.* --ago 0.5h --untagged --concurrency 5
-    retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter .*:^staging.* --ago 0.5h --keep 1 --untagged --concurrency 5
-    retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter .*:^demo.* --ago 0.5h --untagged --concurrency 5
-    retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter .*:^ithc.* --ago 0.5h --untagged --concurrency 5
-    retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter .*:^perftest.* --ago 0.5h --untagged --concurrency 5
-    retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter .*:^sandbox-.* --keep ${keep_min_latest_num} --ago ${nonprod_older_than} --untagged --concurrency 5
-    retries: 3
-  - cmd: acr purge --registry \$RegistryName --filter .*:^pr-.* --ago ${nonprod_older_than} --untagged --concurrency 5
-    retries: 3
-EOF
-  
-# done
+az acr run --registry $registry --cmd "acr purge --registry \$RegistryName --filter ${repo_regex}:${tag_filter} --ago ${older_than} --keep ${keep_min_latest_num}  --untagged --concurrency 5" --timeout 10800  /dev/null
