d5b8cd50fb6ee9cbf51a577c06281beeedaf1ff7
Rees Pozzi <reespozzi@icloud.com>
2023-07-17 10:27:27 +0100

    Add webhook trigger to PR closure cleanup job

--- scripts/delete-hr-on-pr-closure.sh
+++ scripts/delete-hr-on-pr-closure.sh
@@ -8,5 +8,5 @@ az aks get-credentials --resource-group ${azureResourceGroup} --name ${kubernete
 if [ "$(helm get values --namespace "${namespace}" "${release_name}" 2>/dev/null)" ]; then
   helm delete --namespace ${namespace} ${release_name}
 else
-  echo "chart ${release_name} not found, doing nothing"
+  echo "Chart ${release_name} not found."
 fi
