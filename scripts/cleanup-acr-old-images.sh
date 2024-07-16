1acf0ba95256ab69ef06f1683320b3c30ff9e0e3
Charles.Luo <charles.luo1@justice.gov.uk>
2024-07-16 18:33:30 +0100

    added a new attribute of cleanup_patterns represents an array of regExp

--- scripts/cleanup-acr-old-images.sh
+++ scripts/cleanup-acr-old-images.sh
@@ -9,8 +9,20 @@ repo_regex="${2:-.*}"
 tag_filter="${3:-^ignore-.*}"
 older_than="${4:-30d}"
 keep_min_latest_num="${5:-5}"
+repo_tag_filters="${6:['-.*:-^ignore-.*']}"
 
 
 echo "$(TERM=xterm tput setaf 2)Cleaning up $registry, deleting ${tag_filter} images older than $older_than and keeping at least $keep_min_latest_num"
 
-az acr run --registry $registry --cmd "acr purge --registry \$RegistryName --filter ${repo_regex}:${tag_filter} --ago ${older_than} --keep ${keep_min_latest_num}  --untagged --concurrency 5" --timeout 10800  /dev/null
+# iterate repo_tag_filters and build "--filter" arguments
+filter_args=""
+for repo_tag_filter in "${repo_tag_filters[@]}"
+do
+  filter_args="$filter_args --filter $repo_tag_filter"
+done
+
+echo "filter_args: $filter_args"
+
+az acr run --registry $registry \
+ --cmd "acr purge --registry \$RegistryName ${filter_args} --ago ${older_than} --keep ${keep_min_latest_num}  --untagged --concurrency 5" \
+ --timeout 10800  /dev/null
