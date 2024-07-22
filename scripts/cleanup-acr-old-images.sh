e5ad275be93d2a52cfe14ca724bce2acb34cd24c
Charles.Luo <charles.luo1@justice.gov.uk>
2024-07-22 15:36:21 +0100

    test if a comma seperated string is passed in as expected

--- scripts/cleanup-acr-old-images.sh
+++ scripts/cleanup-acr-old-images.sh
@@ -11,11 +11,10 @@ older_than="${4:-30d}"
 keep_min_latest_num="${5:-5}"
 repo_tag_filters="${6:-.*:-^ignore-.*}"
 
-
+echo "repo_tag_filters: $repo_tag_filters"
 echo "$(TERM=xterm tput setaf 2)Cleaning up $registry, deleting ${tag_filter} images older than $older_than and keeping at least $keep_min_latest_num"
 
-repo_tag_filters="^labs/ieuanb74:.*,^labs/jordankainos:.*"
-
+#repo_tag_filters="^labs/ieuanb74:.*,^labs/jordankainos:.*"
 IFS=',' read -r -a repo_tag_array <<< "$repo_tag_filters"
 
 filter_args=""
@@ -25,6 +24,6 @@ done
 
 echo "filter_args: $filter_args"
 
-az acr run --registry $registry \
+az acr run --registry "$registry" \
  --cmd "acr purge --registry \$RegistryName ${filter_args} --ago ${older_than} --keep ${keep_min_latest_num}  --untagged --concurrency 5" \
  --timeout 10800  /dev/null
