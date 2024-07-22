a06160585995f22be8b4b0a4b4f2a6d9eac64b46
Charles.Luo <charles.luo1@justice.gov.uk>
2024-07-22 15:58:23 +0100

    removed redundant parameters repoRegex and tagFiler; added a README file for ACR cleanup

--- scripts/cleanup-acr-old-images.sh
+++ scripts/cleanup-acr-old-images.sh
@@ -5,24 +5,17 @@
 set -e
 
 registry=${1:-hmctspublic}
-repo_regex="${2:-.*}"
-tag_filter="${3:-^ignore-.*}"
-older_than="${4:-30d}"
-keep_min_latest_num="${5:-5}"
-repo_tag_filters="${6:-.*:-^ignore-.*}"
+older_than="${2:-30d}"
+keep_min_latest_num="${3:-5}"
+repo_tag_filters="${4:-.*:-^ignore-.*}"
 
-echo "repo_tag_filters: $repo_tag_filters"
-echo "$(TERM=xterm tput setaf 2)Cleaning up $registry, deleting ${tag_filter} images older than $older_than and keeping at least $keep_min_latest_num"
-
-#repo_tag_filters="^labs/ieuanb74:.*,^labs/jordankainos:.*"
 IFS=',' read -r -a repo_tag_array <<< "$repo_tag_filters"
 
 filter_args=""
 for repo_tag_filter in "${repo_tag_array[@]}"; do
     filter_args="$filter_args --filter $repo_tag_filter"
 done
-
-echo "filter_args: $filter_args"
+echo "$(TERM=xterm tput setaf 2)Cleaning up $registry, deleting [${filter_args}] images older than $older_than and keeping at least $keep_min_latest_num"
 
 az acr run --registry "$registry" \
  --cmd "acr purge --registry \$RegistryName ${filter_args} --ago ${older_than} --keep ${keep_min_latest_num}  --untagged --concurrency 5" \
